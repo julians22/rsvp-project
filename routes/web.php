f59eefba16c0a4b554aa48b048ce9e98b1778f22
Ghassan Fs <fsghassan2429d@gmail.com>
2025-07-28 15:29:52 +0700

    redirect home

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::redirect('/', '/event')->name('home');
 
 Route::group(['prefix' => 'event', 'as' => 'event.'], function () {
 
